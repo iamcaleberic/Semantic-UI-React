041ddaa450614ea8bf40692da2e67c1c16a3c3cc
Levi Thomason <me@levithomason.com>
2017-01-23 18:26:25 -0800

    docs(layouts): stub all layouts

--- docs/app/routes.js
+++ docs/app/routes.js
@@ -1,20 +1,42 @@
 import React from 'react'
 import { Route, IndexRedirect } from 'react-router'
 
+// doc site views
 import Root from './Components/Root'
 import DocsLayout from './Components/DocsLayout'
 import Introduction from './Views/Introduction'
 import Usage from './Views/Usage'
 import Layouts from './Views/Layouts'
-import LoginForm from './Layouts/LoginForm'
 import PageNotFound from './Views/PageNotFound'
+
+// example layouts
+import LayoutsLayout from './Layouts/LayoutsLayout'
+import LoginLayout from './Layouts/LoginLayout'
+import ThemingLayout from './Layouts/ThemingLayout'
+import ResponsiveLayout from './Layouts/ResponsiveLayout'
+import GridLayout from './Layouts/GridLayout'
+import AttachedContentLayout from './Layouts/AttachedContentLayout'
+import BootstrapMigrationLayout from './Layouts/BootstrapMigrationLayout'
+import HomepageLayout from './Layouts/HomepageLayout'
+import StickyLayout from './Layouts/StickyLayout'
+import FixedMenuLayout from './Layouts/FixedMenuLayout'
 
 const routes = (
   <Route path='/'>
     <IndexRedirect to='introduction' />
 
     {/* full page routes */}
-    <Route path='layouts/login' component={LoginForm} />
+    <Route component={LayoutsLayout}>
+      <Route path='layouts/login' component={LoginLayout} />
+      <Route path='/layouts/theming' component={ThemingLayout} />
+      <Route path='/layouts/responsive' component={ResponsiveLayout} />
+      <Route path='/layouts/grid' component={GridLayout} />
+      <Route path='/layouts/attached-content' component={AttachedContentLayout} />
+      <Route path='/layouts/bootstrap-migration' component={BootstrapMigrationLayout} />
+      <Route path='/layouts/homepage' component={HomepageLayout} />
+      <Route path='/layouts/sticky' component={StickyLayout} />
+      <Route path='/layouts/fixed-menu' component={FixedMenuLayout} />
+    </Route>
 
     {/* Docs layout with sidebar */}
     <Route component={DocsLayout}>
